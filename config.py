d6f07c87891e78696f0dc92ccf2798522e85b47d
Dream0526 <1277661840@qq.com>
2018-10-25 19:27:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ class DevelopmentConfig(BaseConfig):
 
     DEBUG = True
     SECRET_KEY = b'0qf\xcd^R\x05\xcem\x88d\x8c\x85~\x05\xe4\\1\xc4\x90\xb3z\xc4e'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/pan'
+    SQLALCHEMY_DATABASE_URI = '*'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     # 文件存储位置
-    UPLOAD_FILE_REPOSITORY = 'C:\\Users\\admin\\Desktop\\restful-pan\\Repository'
+    UPLOAD_FILE_REPOSITORY = '*'
 
 
 class ProductionConfig(BaseConfig):
